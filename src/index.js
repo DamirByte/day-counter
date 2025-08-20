f3f79fcd1fc10149009b906ef4cc632e3a1b4c9b
DamirByte <damirademi.web@gmail.com>
2025-08-20 11:43:05 +0200

    f

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { useState } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return <CounterDate />;
+}
+
+function CounterDate() {
+  const [step, setStep] = useState(5);
+  const [counter, setCounter] = useState(step);
+
+  const message =
+    counter > 0
+      ? "days from today is"
+      : counter === 0
+      ? "Today is"
+      : "days ago was";
+
+  const newDate = new Date(Date.now() + counter * (24 * 60 * 60 * 1000));
+
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <p>Step: {step}</p>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>
+      <div className="wrapper">
+        <button onClick={() => setCounter((c) => c - step)}>-</button>
+        <p>Count: {counter}</p>
+        <button onClick={() => setCounter((c) => c + step)}>+</button>
+      </div>
+      <p>
+        {counter !== 0 && Math.abs(counter)} {message} {newDate.toDateString()}
+      </p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
